2d67b7feb81d63eb22a76bd7a956d14bd016e973
Gaurav Ghosal <gghosal@ucdavis.edu>
2019-04-01 23:03:34 -0700

    Added Crop Out Black Function

--- ImageProcUtil.py
+++ ImageProcUtil.py
@@ -32,8 +32,17 @@ def threshold_dots(imgarray):
     #plt.show()
     
     return img
+def crop_out_black(imagepath):
 
-
+    img = cv2.imread(imagepath)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    _,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+    #print(cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE))
+    img34,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnt = contours[0]
+    x,y,w,h = cv2.boundingRect(cnt)
+    crop = img[y:y+h,x:x+w]
+    return crop
 def threshold_dots_withcenter(imgarray):
     img = imgarray
     hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
