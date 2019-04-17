d0b9e074343c79a463b47b785b949d1c7f04ddf5
Gaurav Ghosal <gghosal@ucdavis.edu>
2019-04-17 00:41:25 -0700

    Create Measurement.py

---
+++ Measurement.py
@@ -0,0 +1,13 @@
+####MeasurementSystem
+import DataStructures
+import pickle
+import pcv
+import csv
+import 
+
+class MeasurementSystem:
+    """Contains executable code for generating measurments from .shelf files"""
+    def __init__(self, measurements_to_create):
+        """measurements_to_create refers to a list of measurements to create"""
+        self.measurements=measurements
+    def 
