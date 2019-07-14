5b48060dbaff3fa46367d432eb88478a9c729a9a
Gaurav Ghosal <gghosal@ucdavis.edu>
2019-07-14 15:00:21 -0700

    Use sqlite3 databases to save the measurements

---
+++ MeasurementSaver.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+class MeasurementSaver:
+    def __init__(self, database_path="/Users/gghosal/Desktop/gaurav_new_photos/measurements.db"):
+        if database_path == None:
+            self.create_database("/Users/gghosal/Desktop/gaurav_new_photos/measurements.db")
+            database_path = "/Users/gghosal/Desktop/gaurav_new_photos/measurements.db"
+        self.connection = sqlite3.connect(database_path)
+        self.cursor = self.connection.cursor()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        self.connection.close()
+
+    def save_singular_measurement(self, date: int, time: int, potnumber: str, position: str, area: float) -> None:
+        self.cursor.execute('''INSERT INTO plants VALUES (?,?,?,?,?)''',
+                            tuple((int(date), int(time), str(potnumber), str(position), float(area))))
+        self.connection.commit()
+
+    def save_multiple_measurements(self, array):
+        """Array should be an array of multiple [[date,time,potnumber, position,area]]"""
+        self.cursor.execute('''INSERT INTO plants VALUES (?,?,?,?,?)''',
+                            array)
+        self.connection.commit()
+
+    def create_database(self, path):
+        conn = sqlite3.connect(path)
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE plants 
+                            (date text, time text, potnumber text, position text, area real''')
+        conn.commit()
